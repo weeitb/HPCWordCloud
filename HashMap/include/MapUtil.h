1a5c569bbdc112632ad46207d0ba93848399019b
Wyatt Bertorelli <wyattbertorelli@gmail.com>
2016-02-18 16:28:31 -0500

    Merged stringMapUtil and MapUtil

--- HashMap/include/MapUtil.h
+++ HashMap/include/MapUtil.h
@@ -1,5 +1,10 @@
 #ifndef MAP_UTIL__H
 #define MAP_UTIL__H
+/**
+ * Utility functions to use Strings as keys for HashMap.
+ * @author Wyatt Bertorelli <wyattbertorelli@gmail.com>
+ */
+#include <stdio.h>
 #include "HashMap.h"
 
 typedef struct MapElements {
@@ -31,5 +36,29 @@ int compareElements(const void* a, const void* b);
  * @param elements array to free
  */
 void deleteMapArray(MapElements* elements uint32_t nElements);
+
+/**
+ * djb2 hashing function invented/reported by Dan Bernstein.
+ * Mysteriously good at generating hashes for character arrays
+ * using the magic number 33.
+ * @param k String key (casted as void*) to hash
+ * @return Hash hash of key
+ */
+Hash StringHasher(Key strK);
+
+/**
+ * Wrapper function for standard c library string comparator. 
+ * @param strKA String A to compare (casted as void*)
+ * @param strKB string B to compare (casted as void*)
+ * @return comparison result.
+ */
+int32_t StringComparator(Key strKA, Key strKB);
+
+/**
+ * Create a new copy of input string.
+ * @param strK String to copy (casted as void*)
+ * @return pointer to new copy (casted as void*)
+ */
+Key StringCopy(Key strK);
 
 #endif /* MAP_UTIL__H */
--- HashMap/src/MapUtil.c
+++ HashMap/src/MapUtil.c
@@ -1,4 +1,11 @@
-#include "stdlib.h"
+/**
+ * Utility functions to use for HashMap.
+ * String functions, as well as conversion of
+ * map to array.
+ * @author Wyatt Bertorelli <wyattbertorelli@gmail.com>
+ */
+#include <stdio.h>
+#include <stdlib.h>
 #include "HashMap.h"
 
 /**
@@ -52,4 +59,43 @@ void deleteMapArray(MapElements* elements uint32_t nElements) {
     free(elements[i]->k);
   }
   free(elements);
+}
+
+
+/**
+ * djb2 hashing function invented/reported by Dan Bernstein.
+ * Mysteriously good at generating hashes for character arrays
+ * using the magic number 33.
+ * @param k String key (casted as void*) to hash
+ * @return Hash hash of key
+ */
+Hash StringHasher(Key k) {
+  Hash hash = 5381;
+  int c;
+  char* str = k;
+  while (c = *str++) {
+    hash = ((hash << 5) + hash) + c;
+  }
+  return hash;
+}
+
+/**
+ * Wrapper function for standard c library string comparator. 
+ * @param strKA String A to compare (casted as void*)
+ * @param strKB string B to compare (casted as void*)
+ * @return comparison result.
+ */
+int32_t StringComparator(Key kA, Key kB) {
+  return strcmp((char*) kA, (char*) kB);
+}
+
+/**
+ * Create a new copy of input string.
+ * @param strK String to copy (casted as void*)
+ * @return pointer to new copy (casted as void*)
+ */
+Key StringCopy(Key strK) {
+  char* newCopy = malloc(sizeof(char) * strlen((char*) k));
+  strcpy(newCopy, (char*) k);
+  return (Key) newCopy;
 }
