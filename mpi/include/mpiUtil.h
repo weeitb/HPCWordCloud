a087bf6d694007f76345839e2a9248701a94e4d6
Wyatt <bertorelli.w@husky.neu.edu>
2016-04-11 23:25:51 -0400

    Finished refactoring Stack. Code needs debugging.

--- mpi/include/mpiUtil.h
+++ mpi/include/mpiUtil.h
@@ -4,58 +4,6 @@
 #include "mpiUtil.h"
 #include "HashMap.h"
 
-/**
- * Element for for filename stack data structure.
- */
-typedef struct FilenameElements {
-  char* filename;
-  struct FilenameElements* nextNode;
-} FilenameElement;
-
-/**
- * Stack data structure for holding list of files to parse.
- * Designed so that each thread or process can pop a file off
- * the stack to process. Mutation of this data structure must 
- * be surrounded with a critical section, so that multiple
- * threads do not end up working on the same files.
- */
-typedef struct FilenameStacks {
-  FilenameElement* front;
-  unsigned int nElements;
-} FilenameStack;
-
-/**
- * Create a new empty stack
- * @return pointer to new allocated stack.
- */
-FilenameStack* newStack();
-
-/**
- * Delete the input stack and all it's elements
- * @param stack stack to free from memory.
- */
-void deleteStack(FilenameStack* stack);
-
-/**
- * push a new filename string onto the stack.
- * Ownership of the filename memory is given
- * to the stack, and should only be free'd after
- * the element has been pushed off the stack.
- * @param stack stack to push onto.
- * @param filename filename to push onto stack.
- */
-void push(FilenameStack* stack, char* filename);
-
-/**
- * Pop the first filename string off of the stack.
- * Ownership of the allocated memory for the filename
- * is given to the process that pops the element off 
- * the stack.
- * @param stack to pop off of.
- * @return filename string. Null if stack empty.
- */
-char* pop(FilenameStack* stack);
-
 union SerializedData {
   uint32_t* intPtr;
   char* strPtr;
--- mpi/src/mpiUtil.c
+++ mpi/src/mpiUtil.c
@@ -6,69 +6,6 @@
 #include "HashMap.h"
 
 /**
- * Create a new empty stack
- * @return pointer to new allocated stack.
- */
-FilenameStack* newStack() {
-  FilenameStack* stack = (FilenameStack*) malloc(sizeof(FilenameStack));
-  stack->front = NULL;
-  stack-> nElements = 0;
-}
-
-/**
- * Delete the input stack and all it's elements
- * @param stack stack to free from memory.
- */
-void deleteStack(FilenameStack* stack) {
-  while (stack->front != NULL) {
-    FilenameElement* current = stack->front;
-    free(current->filename);
-    stack->front = current->nextNode;
-    free(current);
-  }
-  free(stack);
-}
-
-/**
- * push a new filename string onto the stack.
- * Ownership of the filename memory is given
- * to the stack, and should only be free'd after
- * the element has been pushed off the stack.
- * @param stack stack to push onto.
- * @param filename filename to push onto stack.
- */
-void push(FilenameStack* stack, char* filename) {
-  FilenameElement* newElement = (FilenameElement*)malloc(sizeof(FilenameElement));
-  newElement->filename = filename;
-  newElement->nextNode = stack->front;
-  stack->front = newElement;
-}
-
-/**
- * Pop the first filename string off of the stack.
- * Ownership of the allocated memory for the filename
- * is given to the process that pops the element off 
- * the stack.
- * @param stack to pop off of.
- * @return filename string. Null if stack empty.
- */
-char* pop(FilenameStack* stack) {
-  if (stack->front != NULL) {
-    FilenameElement* element;
-    {
-      element = stack->front;
-      stack->front = element->nextNode;
-      stack->nElements--;
-    }
-    char* filename = element->filename;
-    free(element);
-    return filename;
-  } else {
-    return NULL;
-  }
-}
-
-/**
  * Converts a hashmap into a contigious block of memory for
  * sending.
  */
